fc6c114793355e657746e1c02a37a8e52155a41b
Victor Z.T. Feng <victor@solv.finance>
2024-05-13 23:11:15 +0800

    Add github actions scripts.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+let output = 'standalone'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+  output = 'export'
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  output: output,
+}
 
 module.exports = nextConfig

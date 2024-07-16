dd4fa43b32be9baa45dfe9989a9b2ee1f0f1324e
yuyin-zhang <106600353+yuyin-zhang@users.noreply.github.com>
2024-07-16 17:09:54 +1000

    fix: image issue

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -5,7 +5,6 @@ import zCloakLogo from "../public/zCloak-logo.svg";
 import SFTLabsLogo from "../public/SFTLabs.svg";
 import GFILogo from "../public/GFILogo.svg";
 import cofounderlinkedin from "@/public/cofounder-linkedin.svg";
-import TestimonialImage from "@/public/images/TestimonialImage.jpg";
 
 export default function Testimonials() {
   return (
@@ -138,9 +137,9 @@ export default function Testimonials() {
                   >
                     <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
                   </svg>
-                  <Image
+                  <img
                     className="relative rounded-full"
-                    src={TestimonialImage}
+                    src={'/images/TestimonialImage.jpg'}
                     width={96}
                     height={96}
                     alt="Testimonial 01"
